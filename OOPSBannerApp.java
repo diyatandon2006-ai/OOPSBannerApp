db523e7d98424b4d8ce2790d202326e8dc4adb17
Diya Tandon <diyatandon2006@gmail.com>
2026-03-10 13:59:36 +0530

    UC6: Implement banner using Static Functions for O, P and S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,51 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-                String.join("", " ***** "),
-                String.join("", "*     *"),
-                String.join("", "*     *"),
-                String.join("", "*     *"),
-                String.join("", "*     *"),
-                String.join("", "*     *"),
-                String.join("", " ***** ")
-        };
+        String[] o = printO();
+        String[] p = printP();
+        String[] s = printS();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + p[i] + "  " + p[i] + "  " + s[i]);
         }
+    }
+
+    // Pattern for O
+    public static String[] printO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Pattern for P
+    public static String[] printP() {
+        return new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Pattern for S
+    public static String[] printS() {
+        return new String[]{
+                " ***** ",
+                "*     ",
+                "*     ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
     }
 }
