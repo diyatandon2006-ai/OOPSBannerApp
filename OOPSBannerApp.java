6e6ab5413586ace2d2ebdc56291ddd63295f45f3
Diya Tandon <diyatandon2006@gmail.com>
2026-03-17 14:10:19 +0530

    Consolidate UC8 banner and Hello World, remove class files

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,69 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
+
+    static Map<Character, String[]> patternMap = new HashMap<>();
+
+    // Initialize patterns
+    static {
+        patternMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patternMap.put('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        patternMap.put('S', new String[]{
+                " ***** ",
+                "*     ",
+                "*     ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        String bannerWord = "OOPS";
+
+        renderBanner(bannerWord);
+    }
+
+    // Function to render banner
+    public static void renderBanner(String word) {
+
+        int height = 7;
+
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[i]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
     }
 }
